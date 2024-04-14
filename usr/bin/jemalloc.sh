6dda4cd9aac4e9dd17712b05b3925c939ae248c4
Feng Feng <F.Feng@outlook.com>
2024-04-14 10:12:38 +0800

    make clean

--- usr/bin/jemalloc.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-prefix=/Volumes/DATA/Projects/FIREs/usr
-exec_prefix=/Volumes/DATA/Projects/FIREs/usr
-libdir=${exec_prefix}/lib
-
-DYLD_INSERT_LIBRARIES=${libdir}/libjemalloc.2.dylib
-export DYLD_INSERT_LIBRARIES
-exec "$@"
